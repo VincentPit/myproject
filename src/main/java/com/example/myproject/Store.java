cc4ff7e61906b892f585b4656d62b14c048f8232
Stephen Li <jl13122@nyu.edu>
2023-11-26 14:46:36 -0500

    first commit

---
+++ src/main/java/com/example/myproject/Store.java
@@ -0,0 +1,28 @@
+package com.example.myproject;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<User> users;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Transaction> transactions;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Merchandise> inventory;
+
+    public Store() {
+        this.users = new ArrayList<>();
+        this.transactions = new ArrayList<>();
+        this.inventory = new ArrayList<>();
+    }
+
+}
---
+++ src/main/java/com/example/myproject/Transaction.java
@@ -0,0 +1,24 @@
+package com.example.myproject;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime time;
+    private double amount;
+
+    @ManyToOne
+    private Merchandise merchandise;
+
+    public Transaction(LocalDateTime time, double amount, Merchandise merchandise) {
+        this.time = time;
+        this.amount = amount;
+        this.merchandise = merchandise;
+    }
+
+}
