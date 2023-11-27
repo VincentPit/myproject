ab78e9a6c966576059610e4112e1be0e83e08e7f
Stephen Li <jl13122@nyu.edu>
2023-11-27 17:18:54 -0500

    include h2 database

--- src/main/java/com/example/myproject/App.java
+++ src/main/java/com/example/myproject/App.java
@@ -1,9 +1,39 @@
 package com.example.myproject;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.example.myproject.model.H2DatabaseServer;
+import com.example.myproject.model.User;
+import java.util.concurrent.CountDownLatch;
+
+public class App {
+    public static void main(String[] args) {
+        CountDownLatch latch = new CountDownLatch(1);
+
+        Thread serverThread = new Thread(() -> {
+            try {
+                System.out.println("Before starting H2 Database Server");
+                H2DatabaseServer.startServer();
+
+                System.out.println("After starting H2 Database Server");
+                latch.countDown(); // Signal that the server has been initialized
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        serverThread.start();
+
+        try {
+            // Wait for the server thread to initialize the server
+            latch.await();
+
+            User a = new User();
+            // App logic continues after the server is initialized
+            System.out.println("App logic continues...");
+
+            // Rest of your App.java logic
+            System.out.println("Hello World!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/example/myproject/model/H2DatabaseServer.java
+++ src/main/java/com/example/myproject/model/H2DatabaseServer.java
@@ -4,22 +4,24 @@ import org.h2.tools.Server;
 
 public class H2DatabaseServer {
 
-    public static void main(String[] args) throws Exception {
-        //Start H2 TCP server
-        Server server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
-        
-        System.out.println("H2 Database Server is running on port " + server.getPort());
+    public static void startServer() {
+        try {
+            Server server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092", "-trace").start();
+            System.out.println("H2 Database Server is running on port " + server.getPort());
 
-        //Optionally start the H2 web console
-        //Open your web browser and navigate to http://localhost:8082
-        Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
-        
-        System.out.println("H2 Web Console is running on port 8082");
+            // Optionally start the H2 web console
+            // Open your web browser and navigate to http://localhost:8082
+            Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
 
-        //shutdown the server when the program exits
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("Shutting down H2 Database Server");
-            server.stop();
-        }));
+            System.out.println("H2 Web Console is running on port 8082");
+
+            // Shutdown the server when the program exits
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Shutting down H2 Database Server");
+                server.stop();
+            }));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
